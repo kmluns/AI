ff201ceb1a98b185fb02eb43a8e8ce01a5dc8c23
kmluns <kamil.unsal@icterra.com>
2019-08-28 11:05:42 +0300

    chess project fixed

--- DeepQLearning/chess/chess.py
+++ DeepQLearning/chess/chess.py
@@ -18,6 +18,11 @@ from keras.models import Sequential
 from keras.optimizers import Adam
 from keras.layers import Dense, Dropout, Flatten, Conv1D, MaxPool1D
 
+# save model
+from keras.models import model_from_json
+
+#graph
+import matplotlib.pyplot as plt
 
 
 # %% class DQL Agent
@@ -34,42 +39,61 @@ class DQLAgent:
         
         self.epsilon = 1
         self.epsilon_min = 0.01
-        self.epsilon_decay = 0.995
+        self.epsilon_decay = 0.99
         
-        self.memory = deque(maxlen = 100000)
+        self.memory = deque(maxlen = 100)
         
         self.model = self.build_model()
         self.target_model = self.build_model()
+        
+        self.random = 0
+        self.action = 0
+        self.notAction = 0
+        self.forcedAction =0
+        self.resign = 0
+        
+        
+        # serialize model to JSON
+        model_json = self.model.to_json()
+        with open("model/big/model.json", "w") as json_file:
+            json_file.write(model_json)
 
 
     
     def build_model(self):
         model = Sequential()
-        model.add(Conv1D(filters = 512, kernel_size = (5),padding = 'Same', 
-                         activation ='relu', input_shape = self.state_shape))
-        model.add(MaxPool1D(pool_size=(2)))
-        model.add(Dropout(0.25))
-        #
-        model.add(Conv1D(filters = 512, kernel_size = (3),padding = 'Same', 
-                         activation ='relu'))
-        model.add(MaxPool1D(pool_size=(2), strides=(2)))
-        model.add(Dropout(0.5))
-        model.add(Conv1D(filters = 128, kernel_size = (3),padding = 'Same', 
-                         activation ='relu', input_shape = self.state_shape))
-        model.add(MaxPool1D(pool_size=(2)))
-        model.add(Dropout(0.25))
-        model.add(Conv1D(filters = 128, kernel_size = (3),padding = 'Same', 
-                         activation ='relu', input_shape = self.state_shape))
-        model.add(Dropout(0.8))
-        model.add(Conv1D(filters = 64, kernel_size = (3),padding = 'Same', 
-                         activation ='relu', input_shape = self.state_shape))
-        model.add(Dropout(0.25))
+# =============================================================================
+#         model.add(Conv1D(filters = 512, kernel_size = (5),padding = 'Same', 
+#                          activation ='relu', input_shape = self.state_shape))
+#         model.add(MaxPool1D(pool_size=(2)))
+#         model.add(Dropout(0.25))
+#         #
+#         model.add(Conv1D(filters = 512, kernel_size = (3),padding = 'Same', 
+#                          activation ='relu'))
+#         model.add(MaxPool1D(pool_size=(2), strides=(2)))
+#         model.add(Dropout(0.5))
+#         model.add(Conv1D(filters = 128, kernel_size = (3),padding = 'Same', 
+#                          activation ='relu', input_shape = self.state_shape))
+#         model.add(MaxPool1D(pool_size=(2)))
+#         model.add(Dropout(0.25))
+#         model.add(Conv1D(filters = 128, kernel_size = (3),padding = 'Same', 
+#                          activation ='relu', input_shape = self.state_shape))
+#         model.add(Dropout(0.8))
+#         model.add(Conv1D(filters = 64, kernel_size = (3),padding = 'Same', 
+#                          activation ='relu', input_shape = self.state_shape))
+#         model.add(Dropout(0.25))
+# =============================================================================        
         # fully connected
+        
+        #model.add(Dense(256, activation='relu'))
+        #model.add(Dropout(0.8))
+        model.add(Dense(256, input_shape= self.state_shape, activation='relu'))
         model.add(Flatten())
+        model.add(Dense(64, activation='relu'))
+        model.add(Dropout(0.25))
+        model.add(Dense(64, activation='relu'))
+        model.add(Dropout(0.25))
         model.add(Dense(256, activation='relu'))
-        model.add(Dropout(0.8))
-        model.add(Dense(64, activation='relu'))
-        model.add(Dense(64, activation='relu'))
         model.add(Dense(self.action_size, activation='linear'))
         model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
         return model
@@ -80,20 +104,36 @@ class DQLAgent:
     def adaptiveEGready(self):
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
-
-    def select_action(self,state, player):  
+            
+    def select_action(self, state, player):  
+        resign = False
         actions = self.env.get_possible_actions(self.env.state, player)
         if len(actions) == 0:
-            return self.env.resign_action()
+            self.resign +=1
+            resign = True
+            return self.env.resign_action(), resign
         # acting : explore or exploit
         if random.uniform(0,1) <= self.epsilon:
             #explore
-            return random.choice(actions)
+            self.random += 1
+            return random.choice(actions), resign
         # exploit
         act_values = self.model.predict(state.reshape(-1,8,8))
-        predicted_actions = act_values[0]
-        action_index = np.argmax(predicted_actions[actions])
-        return actions[action_index]
+        #predicted_actions = act_values[0]
+        #action_index = np.argmax(predicted_actions[actions])
+        #return actions[action_index]
+        action = np.argmax(act_values[0])
+        if action in actions:
+            self.action += 1
+        else:
+            if self.notAction > self.forcedAction /2:
+                predicted_actions = act_values[0]
+                action_index = np.argmax(predicted_actions[actions])
+                action = actions[action_index]
+                self.forcedAction += 1
+            else:
+                self.notAction += 1
+        return action, resign
         
     def replay(self,batch_size):
         "vectorized replay method"
@@ -142,11 +182,18 @@ class DQLAgent:
         
     def targetModelUpdate(self):
         self.target_model.set_weights(self.model.get_weights())
-        self.target_model.save_weights("model/target_model.h5")
+        self.target_model.save_weights("model/big/target_model_acc.h5")
+        print("action : {}, notaction : {}, random : {}, forcedAction : {}, resign : {} ".format(self.action, self.notAction, self.random, self.forcedAction, self.resign))
+        self.random = 0
+        self.action = 0
+        self.notAction = 0
+        self.forcedAction = 0
+        self.resign = 0
+
         
     def model_save_best(self, reward):
-        self.model.save_weights("model/best_model"+reward+".h5")
-        self.target_model.save_weights("model/best_target_model"+reward+".h5")
+        self.model.save_weights("model/big/best_model"+reward+".h5")
+        self.target_model.save_weights("model/big/best_target_model"+reward+".h5")
         
     def model_load(self,filePath):
         self.model.load_weights(filePath)
@@ -158,13 +205,13 @@ env = gym.make('ChessVsRandomBot-v0')
 agent = DQLAgent(env)
 
 
-agent.model_load("model/target_model.h5")
+#agent.model_load("model/target_model_acc.h5")
 
 # %% main function
 if __name__ == "__main__":
     
     # initialize env and agent
-    batch_size = 1024
+    batch_size = 64
     
     
     player = 1
@@ -172,6 +219,13 @@ if __name__ == "__main__":
     
     best_total_reward = 0
     episodes = 10000
+    
+    rewards = []
+    
+    plt.plot(rewards)
+    plt.ylabel('rewards')
+    plt.show()
+    
     for e in range(episodes):
         
         # initialize environment
@@ -181,18 +235,33 @@ if __name__ == "__main__":
         
         
         total_reward = 0
+        step_number = 0
+        
         
         while True:
             #env._render()
 
             # act
-            action = agent.select_action(state, player)
-                      
-            # step
-            next_state, reward, done, _ = env.step(action)
-            next_state = next_state['board'].reshape(-1,8,8)
+            action, resign = agent.select_action(state, player)
             
-            total_reward += reward
+            actions = env.get_possible_actions(env.state, player)
+            
+            next_state, reward, done = state, -1000, False
+            
+            if resign:
+                reward, done = -100, True
+                total_reward += reward
+            
+            elif action in actions:
+                #print("not in action : {}".format(action))
+                      
+                # step
+                next_state, reward, done, _ = env.step(action)
+                if reward == 0:
+                    reward = -0.0001
+                next_state = next_state['board'].reshape(-1,8,8)
+                total_reward += reward
+            
             
             # remember / storage
             agent.remember(state, action, reward, next_state, done)
@@ -206,15 +275,21 @@ if __name__ == "__main__":
             # adjust epsilon
             agent.adaptiveEGready()
             
-            if done:
-                print('Episode {}, reward {}'.format(e,total_reward))
-                agent.targetModelUpdate()
-                break
+            step_number += 1
             
             if best_total_reward < total_reward:
                 best_total_reward = total_reward
-                agent.model_save_best(best_total_reward)
+                agent.model_save_best(str(int(best_total_reward)))
             
+            if done or total_reward < -100 or step_number > 500:
+                print('Episode {}, reward {}, step {}'.format(e,total_reward, step_number))
+                agent.targetModelUpdate()
+                rewards.append(total_reward)
+                break
+            
+            
+        plt.plot(rewards)
+        plt.pause(0.05)
                 
                 
 # %% test
